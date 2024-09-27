b82de3f3cab77a8a622851b9e2496409779ad13f
thenew8bitducky <marcop13121312@gmail.com>
2024-09-27 15:03:44 -0400

    Update install_this.sh

--- install_this.sh
+++ install_this.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 
-pacman -S spectacle firefox dolphin-emu zim nemo chromium htop nvidia kitty neofetch discord thunar calligra flatpak adobe-source-han-sans-jp-fonts bittwarden thunderbird obs-studio  --noconfirm
+pacman -S spectacle firefox dolphin-emu zim nemo chromium htop nvidia kitty neofetch discord thunar calligra flatpak adobe-source-han-sans-jp-fonts bittwarden steam  thunderbird obs-studio  --noconfirm
 
 
 git clone https://aur.archlinux.org/yay.git
@@ -15,3 +15,8 @@ flatpak install flathub org.ryujinx.Ryujinx -y
 flatpak install flathub com.spotify.Client -y
 flatpak install flathub us.zoom.Zoom -y
 flatpak install flathub org.nickvision.tubeconverter -y
+
+
+git clone https://github.com/ChrisTitusTech/Top-5-Bootloader-Themes
+cd Top-5-Bootloader-Themes
+sudo ./install.sh
