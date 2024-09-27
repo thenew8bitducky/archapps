531356935f4723d9079007601ae78d708cdb53bc
thenew8bitducky <marcop13121312@gmail.com>
2024-09-27 15:19:07 -0400

    Update install_this.sh

--- install_this.sh
+++ install_this.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/bash
 
 
-pacman -S spectacle firefox dolphin-emu zim nemo chromium htop nvidia kitty neofetch discord thunar calligra flatpak adobe-source-han-sans-jp-fonts bittwarden steam  thunderbird obs-studio  --noconfirm
+pacman -S spectacle firefox dolphin-emu zim nemo chromium yazi powerline-fonts htop nvidia kitty neofetch discord thunar calligra flatpak adobe-source-han-sans-jp-fonts bittwarden steam  thunderbird obs-studio  --noconfirm
 
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+
+git clone --recursive https://github.com/andresgongora/synth-shell.git
+
+git clone https://github.com/ChrisTitusTech/Top-5-Bootloader-Themes
+
 
 yay -S krabby-bin
 
@@ -15,8 +20,5 @@ flatpak install flathub org.ryujinx.Ryujinx -y
 flatpak install flathub com.spotify.Client -y
 flatpak install flathub us.zoom.Zoom -y
 flatpak install flathub org.nickvision.tubeconverter -y
-
-
-git clone https://github.com/ChrisTitusTech/Top-5-Bootloader-Themes
-cd Top-5-Bootloader-Themes
-sudo ./install.sh
+flatpak install flathub info.febvre.Komikku -y
+flatpak install flathub com.georgefb.mangareader -y
