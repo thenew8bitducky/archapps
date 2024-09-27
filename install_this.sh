8350b4a6c843a0fa4005ecd8978c9071471e35ed
thenew8bitducky <marcop13121312@gmail.com>
2024-09-26 23:11:59 -0400

    Update install_this.sh

--- install_this.sh
+++ install_this.sh
@@ -1,4 +1,26 @@
 #!/usr/bin/bash
 
 
-pacman -S spectacle firefox zim kitty neofetch discord thunar adobe-source-han-sans-jp-fonts bittwarden thunderbird obs-studio  --noconfirm
+pacman -S spectacle firefox zim nvidia kitty neofetch discord thunar calligra flatpak adobe-source-han-sans-jp-fonts bittwarden thunderbird obs-studio  --noconfirm
+
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+yay -S krabby-bin
+
+
+
+
+
+
+
+
+
+
+
+flatpak install flathub org.ryujinx.Ryujinx -y
+flatpak install flathub com.spotify.Client -y
+flatpak install flathub us.zoom.Zoom -y
+flatpak install flathub org.nickvision.tubeconverter -y
