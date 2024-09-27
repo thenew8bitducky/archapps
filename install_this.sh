a2bd34e2a2c8efc70230c1ddb601c7034e030dba
thenew8bitducky <marcop13121312@gmail.com>
2024-09-26 22:57:14 -0400

    Update install_this.sh

--- install_this.sh
+++ install_this.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 
-pacman -S spectacle firefox zim neofetch discord thunar adobe-source-han-sans-jp-fonts bittwarden thunderbird obs-studio  --noconfirm
+pacman -S spectacle firefox zim kitty neofetch discord thunar adobe-source-han-sans-jp-fonts bittwarden thunderbird obs-studio  --noconfirm
