a56fe19cb6fddee6216f64a95e94e69deefa8c9d
thenew8bitducky <marcop13121312@gmail.com>
2024-09-27 14:57:40 -0400

    Update install_this.sh

--- install_this.sh
+++ install_this.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 
-pacman -S spectacle firefox zim nvidia kitty neofetch discord thunar calligra flatpak adobe-source-han-sans-jp-fonts bittwarden thunderbird obs-studio  --noconfirm
+pacman -S spectacle firefox zim nemo chromium htop nvidia kitty neofetch discord thunar calligra flatpak adobe-source-han-sans-jp-fonts bittwarden thunderbird obs-studio  --noconfirm
 
 
 git clone https://aur.archlinux.org/yay.git
@@ -9,15 +9,6 @@ cd yay
 makepkg -si
 
 yay -S krabby-bin
-
-
-
-
-
-
-
-
-
 
 
 flatpak install flathub org.ryujinx.Ryujinx -y
