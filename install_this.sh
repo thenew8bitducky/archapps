8cdfe74f6c13961b372bed256dad09a85ac048fd
thenew8bitducky <marcop13121312@gmail.com>
2024-11-14 01:20:22 -0500

    Update install_this.sh

--- install_this.sh
+++ install_this.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 
 
-pacman -S spectacle firefox dolphin-emu zim nemo chromium yazi powerline-fonts htop nvidia alacritty  neofetch discord thunar calligra flatpak adobe-source-han-sans-jp-fonts bittwarden steam  thunderbird obs-studio cosmic hyprland awesome rofi waybar     --noconfirm
+pacman -S  firefox dolphin-emu zim nemo chromium yazi powerline-fonts htop nvidia alacritty  neofetch discord thunar flatpak adobe-source-han-sans-jp-fonts bittwarden steam  thunderbird obs-studio cosmic hyprland awesome rofi waybar     --noconfirm
 
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-git clone --recursive https://github.com/andresgongora/synth-shell.git
+# git clone --recursive https://github.com/andresgongora/synth-shell.git
 sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"
 git clone https://github.com/ChrisTitusTech/Top-5-Bootloader-Themes
 
