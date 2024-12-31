b425828e8c130c0b711e00e0a314fff362b37e10
thenew8bitducky <marcop13121312@gmail.com>
2024-12-31 01:13:13 -0500

    Update install_this.sh

--- install_this.sh
+++ install_this.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/bash
 
 
-pacman -S  firefox dolphin-emu zim nemo chromium yazi powerline-fonts htop nvidia alacritty  neofetch discord thunar flatpak adobe-source-han-sans-jp-fonts bittwarden steam  thunderbird obs-studio cosmic hyprland awesome rofi waybar     --noconfirm
+pacman -S  firefox dolphin-emu zim nemo chromium yazi powerline-fonts htop nvidia alacritty hyfetch neofetch discord thunar flatpak adobe-source-han-sans-jp-fonts bittwarden steam  thunderbird cosmic hyprland awesome rofi waybar micro      --noconfirm
 
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-
-# git clone --recursive https://github.com/andresgongora/synth-shell.git
-sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"
-git clone https://github.com/ChrisTitusTech/Top-5-Bootloader-Themes
 
 
 yay -S krabby-bin
@@ -23,3 +19,4 @@ flatpak install flathub org.nickvision.tubeconverter -y
 flatpak install flathub info.febvre.Komikku -y
 flatpak install flathub com.georgefb.mangareader -y
 flatpak install flathub app.ytmdesktop.ytmdesktop -y
+flatpak install flathub com.obsproject.Studio -y
